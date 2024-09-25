a4cf1be2a9d5b08a484ed652be222d92afff5735
Talha Irfan <irfantalha683@gmail.com>
2024-09-25 12:51:01 +0500

    new

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-var str = "this IS JAVA SCRIPT"
-var a = str.toUpperCase();
-document.write(a);
+let counter = 0;
+    
+function increment() {
+    counter++;
+    document.getElementById("Null").innerText = counter;
+    document.getElementById("Null").style.fontSize = "x-large";
+}
+
+function decrement() {
+    counter--;
+    document.getElementById("Null").innerText = counter;
+}
